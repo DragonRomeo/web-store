e3f62906dbb9132ee3bb11cff4fd6d78e474c968
Ilya Dragonromeo <you@example.com>
2023-08-09 17:54:29 +0300

    feat: add variablies scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 //rfc/rafce - for fast snippet.
 //ctrl + alt + Rimport React from 'react'
 import styles from './Header.module.css';
-
+// import svg from '../../../public/logo2.png';
 
 const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <div>logo</div>
+       <h1 className={styles.h1}>online-shop</h1>
         <nav className={styles.nav}>
           <ul className={styles.ul}>
             <li>Home</li>
