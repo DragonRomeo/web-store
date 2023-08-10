b119609a3e9e77f57b0abffc0d38dd61b49bb74c
Ilya Dragonromeo <you@example.com>
2023-08-10 15:57:04 +0300

    feat: add games.data.js

---
+++ games.data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        id:0,
+        name: 'God of War',
+        brand: 'Sony',
+        price: 69.99,
+        image: '/filename.png'
+    },
+    {
+        id: 1,
+        name: 'Mortal Combat 11',
+        brand: 'Warner Brothers',
+        price: 8.99,
+        image: ''
+    },
+    {
+        id: 2,
+        name: 'Jedi Survivor',
+        brand: 'EA',
+        price: 79.99,
+        image: ''
+    },
+    {
+        id: 3,
+        name: 'Resident Evil 2',
+        brand: 'Capcom',
+        price: 9.99,
+        image: ''
+    },
+    {
+        id: 4,
+        name: 'Uncharted',
+        brand: 'Sony',
+        price: 18.99,
+        image: ''
+    },
+]
+
+export default data;
