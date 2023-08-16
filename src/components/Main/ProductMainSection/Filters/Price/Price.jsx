eb1c21c4695b3967bd219d9f0b4ca52dc7d10ca7
Ilya Dragonromeo <you@example.com>
2023-08-16 19:43:32 +0300

    feat: add Price

--- src/components/Main/ProductMainSection/Filters/Price/Price.jsx
+++ src/components/Main/ProductMainSection/Filters/Price/Price.jsx
@@ -1,7 +1,11 @@
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <div>
+      <h5>price</h5>
+      <p>100$</p>
+      <input type="range" name="price" min='0' max='100' />
+    </div>
   )
 }
 
