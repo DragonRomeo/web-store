9c9e01f5c652f27a71264a49ccf5f67df87434df
Ilya Dragonromeo <you@example.com>
2023-08-16 19:35:14 +0300

    feat: add Category filter

--- src/components/Main/ProductMainSection/Filters/Category/Category.jsx
+++ src/components/Main/ProductMainSection/Filters/Category/Category.jsx
@@ -1,8 +1,18 @@
+import styles from './Category.module.css';
 
 const Category = () => {
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h5 className={styles.h5}>category</h5>
+      <div className={styles.btnWrapper}>
+        <button>rpg</button>
+        <button>action</button>
+        <button>shooter</button>
+        <button>simulator</button>
+        <button>strategy</button>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
