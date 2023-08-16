84422c3c67f8f15f2542df871f55f6790f33304b
Ilya Dragonromeo <you@example.com>
2023-08-16 15:45:01 +0300

    feat: update sort styles in options

--- src/components/Main/ProductMainSection/ProductsContainer/Options/Sort/Sort.jsx
+++ src/components/Main/ProductMainSection/ProductsContainer/Options/Sort/Sort.jsx
@@ -1,8 +1,17 @@
+import styles from './Sort.module.css'
 
 const Sort = () => {
   return (
-    <div>Sort</div>
-  )
-}
+    <form className={styles.form}>
+      <label htmlFor='sort'>sort by</label>
+      <select className={styles.select} name='sort' id='sort'>
+        <option value='price-lowest'>price (lowest)</option>
+        <option value='price-highest'>price (highest)</option>
+        <option value='name-a'>name (a - z)</option>
+        <option value='name-z'>name (z - a)</option>
+      </select>
+    </form>
+  );
+};
 
-export default Sort
+export default Sort;
