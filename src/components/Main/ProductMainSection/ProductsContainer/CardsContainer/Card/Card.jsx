6bbc5bfbecd0fe052044e3834c30fbb2297ec78c
Ilya Dragonromeo <you@example.com>
2023-08-17 17:18:18 +0300

    feat: add url for json

--- src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/Card/Card.jsx
+++ src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/Card/Card.jsx
@@ -1,9 +1,21 @@
 import styles from './Card.module.css'
 //rafce
+const getUrl = async () => {
+  const url = 'https://dummyjson.com/products';
+  const res = await fetch(url);
+  const json = await res.json();
+  console.log(json);
+  console.log(json.products);
+}
+
+const handleClick = () => {
+  console.log('click');
+  getUrl();
+}
 
 const Card = () => {
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleClick}>
       <img src="https://i.playground.ru/e/zGJdOLw2YeyxUg2offNtjg.jpeg?1200x1200" width="300px" height="300px" alt="img" />
       <div className={styles.info}>
       <p>Game Name</p>
--- src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/CardsContainer.jsx
+++ src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/CardsContainer.jsx
@@ -1,10 +1,23 @@
 import Card from './Card/Card';
 import styles from './CardsContainer.module.css'
 
+// const url = 'https://raw.githubusercontent.com/DragonRomeo/online-store-server/main/games.data.json';
+const getUrl = async () => {
+  const url = 'https://dummyjson.com/products';
+  const res = await fetch(url);
+  const json = await res.json();
+  console.log(json);
+}
+
+const handleClick = () => {
+  console.log('click')
+}
+
+
 const CardsContainer = () => {
   return (
     <div className={styles.container}>
-      <Card />
+      <Card onClick={handleClick} />
       <Card />
       <Card />
       <Card />
