94aefc805478e19e65baea191b559eff36f43e36
Ilya Dragonromeo <you@example.com>
2023-08-18 16:09:42 +0300

    feat: add price in card

--- src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/Card/Card.jsx
+++ src/components/Main/ProductMainSection/ProductsContainer/CardsContainer/Card/Card.jsx
@@ -2,29 +2,17 @@ import styles from './Card.module.css';
 import PropTypes from 'prop-types';
 //rafce
 
-// const Card = ({ value }) => {
-//   return (
-//     <div className={styles.card}>
-//       {/* <img src={value.thumbnail} width='300px' height='300px' alt='img' /> */}
-//       <div className={styles.info}>
-//         <p>{value.title}</p>
-//         <p> {new Intl.NumberFormat('en-US', {
-//             style: 'currency',
-//             currency: 'USD',
-//           }).format(value.price)}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
 const Card = ({ value }) => {
-  if (typeof value !== 'object') return;
-  
-  console.log(value);
   return (
     <div className={styles.card} key={value.id}>
       <img src={value.images[0]} width='300px' height='300px' alt='img' />
       <p>{value.title}</p>
+      <p>
+        {new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(value.price)}
+      </p>
     </div>
   );
 };
