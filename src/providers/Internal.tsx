310eadc91aac04fd1388c23dde957c4c31416959
Ilya Dragonromeo <you@example.com>
2023-09-19 19:59:38 +0300

    refactor: delete extra test files

--- src/providers/Internal.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { MyContext } from './MyContext';
-
-export const Internal = ():string => {
-  const context = useContext(MyContext);
-    console.log(`context is = ${context}`)
-  return `bla bla bla bla this is ${context} `
-};
